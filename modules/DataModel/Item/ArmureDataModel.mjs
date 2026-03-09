a971e3fb745407e9f212b0738ac2b019dd3c635c
eltharin <eltharin18@hotmail.fr>
2026-03-09 13:51:01 +0100

    remplace reduction par reductionDegat + bonusDefense dans armure

--- modules/DataModel/Item/ArmureDataModel.mjs
+++ modules/DataModel/Item/ArmureDataModel.mjs
@@ -9,14 +9,21 @@ export class ArmureDataModel extends foundry.abstract.TypeDataModel {
 
   static defineSchema() {
     return {
+      isEquipe: new foundry.data.fields.BooleanField({initial: false}),
       prixmoyen: new foundry.data.fields.NumberField({initial: 0, min:0}),
       type: new foundry.data.fields.StringField({}),
-      reduction: new foundry.data.fields.StringField({}),
+      reduction: new foundry.data.fields.StringField({}), // TODO: remove in next major version
+      reductionDegat: new foundry.data.fields.NumberField({}),
+      bonusDefense: new foundry.data.fields.NumberField({initial: 0}),
       inconvenient: new foundry.data.fields.StringField({}),
     };
   }
 
   prepareDerivedData() {
       this.prix = Helpers.Argent.convertAtoB(this.prixmoyen);
+      if(this.reductionDegat == undefined && this.reduction != undefined) // TODO: remove in next major version
+      {
+        this.reductionDegat = this.reduction;
+      }
   }
 }
